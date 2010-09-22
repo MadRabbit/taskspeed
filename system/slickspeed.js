a74fd872c0bc8ff8c96fb5f626a8e0a39f756091
Peter Galiba <poetro@poetro.hu>
2010-09-22 22:13:41 +0200

    Added RightJS 2.0 support, and fixed error reporting bug.

--- system/slickspeed.js
+++ system/slickspeed.js
@@ -7,31 +7,31 @@ function forEach(iterable, fn, bind){
 //test start
 
 window.onload = function(){
-	
+
 	var frameworks = {};
-	
+
 	forEach(document.getElementsByTagName('iframe'), function(iframe){
 		frameworks[iframe.name] = {
 			'test': iframe.contentWindow.test,
 			'selectors': []
 		};
 	});
-	
+
 	var tbody = document.getElementById('tbody');
 	var tfoot = document.getElementById('tfoot');
 	var lastrow = tfoot.getElementsByTagName('tr')[0];
 	var rows = [];
-	
+
 	var controls = document.getElementById('controls');
-	
+
 	var links = controls.getElementsByTagName('a');
-	
+
 	var start = links[1];
 	var stop = links[0];
-	
+
 	start.onclick = function(){
 		// randomize tests within rows
-		tests.sort(function(a,b) { 
+		tests.sort(function(a,b) {
 			if (a.row_num == b.row_num)
 				return Math.random() < 0.5 ? 1: -1
 			else
@@ -40,16 +40,16 @@ window.onload = function(){
 		testRunner();
 		return false;
 	};
-	
+
 	stop.onclick = function(){
 		clearTimeout(timer);
 		timer = null;
 		return false;
 	};
-	
+
 	var score = [];
 	var scores = {};
-	
+
 	var frxi = 0;
 	for (var name in frameworks){
 		var framework = frameworks[name];
@@ -60,7 +60,7 @@ window.onload = function(){
 		score[name] = 0;
 		frxi++;
 	}
-	
+
 	var tests = [];
 
 	forEach(window.selectors, function(selector, i){
@@ -82,10 +82,10 @@ window.onload = function(){
 			frxi++;
 		}
 	});
-	
+
 	var submitRun = false;
 	var submitTest = function(data){
-		
+
 		if(!data || !confirm("Yo! Click OK to send the results back for charting")){
 			return;
 		}
@@ -94,13 +94,13 @@ window.onload = function(){
 		for(var i in data){
 			pay.scores[i] = data[i].innerHTML.match(/[0-9]+/)[0];
 		}
-		
+
 		var s = document.createElement('script');
 		s.src = "frameworks/dojo-131.js";
 		var h = document.getElementsByTagName("head")[0];
 		s.onload = s.onreadystatechange = function(e){
 			if((e && e.type == "load") || /loaded|complete/.test(s.readyState)){
-				dojo.xhrPost({ 
+				dojo.xhrPost({
 					url:"report.php",
 					content: { data: dojo.toJson(pay) },
 					load:function(){
@@ -111,12 +111,12 @@ window.onload = function(){
 		}
 		h.appendChild(s);
 	}
-	
+
 	var timer = null;
-	
+
 	var testRunner = function(){
 		var test = tests.shift();
-		if(!test){ 
+		if(!test){
 			submitTest(scores);
 			return;
 		}
@@ -125,57 +125,58 @@ window.onload = function(){
 		test.cell.innerHTML = '<b>' + results.time + ' ms</b><b>' + results.found + ' found</b>';
 		test.cell.speed = results.time;
 		if (results.error){
-			test.cell.innerHTML = results.time + ' ms | <span class="exception" title="' + results.error + '">error returned</a>';
+			test.cell.innerHTML = results.time + ' ms | <span class="exception">error returned</a>';
 			test.cell.className += ' exception';
 			test.cell.found = 0;
 			test.cell.error = true;
+      test.cell.title = results.error;
 		} else {
 			test.cell.found = results.found;
 			test.cell.error = false;
 		}
-		
+
 		results.code = results.code.toString().replace(/^.*\{/,'').replace(/\s*\}\s*$/,'');
 		results.code = results.code.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/\t/g,'&nbsp;&nbsp;');
-		
+
 		test.cell.innerHTML = ['<a class="code"><pre>', results.code, '</pre></a>', test.cell.innerHTML].join('');
-		
+
 		test.cell.onclick = function(){
 			if (/showcode/.test(test.cell.className))
 				test.cell.className = test.cell.className.replace(' showcode','');
 			else
 				test.cell.className += ' showcode';
 		}
-		
+
 		score[test.name] += test.cell.speed;
 		scores[test.name].innerHTML =  '&nbsp;' + score[test.name] + '&nbsp;';
-		
+
 		rows[test.row_num]--;
 		if(rows[test.row_num] == 0) colourRow(test.row);
 		timer = setTimeout(testRunner, 125);
 	};
-	
+
 	var colourRow = function(row){
-		
+
 		var cells = [];
-		
+
 		var tds = row.getElementsByTagName('td');
 		forEach(tds, function(td){
 			cells.push(td);
 		});
-		
+
 		var speeds = [];
-		
+
 		forEach(cells, function(cell, i){
 			if (!cell.error) speeds[i] = cell.speed;
 			//error, so we exclude it from colouring good. does not affect score (it should?).
 			else speeds[i] = 99999999999999999999999;
 		});
-		
+
 		var min = Math.min.apply(this, speeds);
 		var max = Math.max.apply(this, speeds);
 		// FIXME: is this a good way? should we mean the speeds? median?
 		var threshold = min; // Math.floor(min / 2); // ms
-		
+
 		var found = [];
 		var mismatch = false;
 		forEach(cells, function(cell, i){
@@ -188,21 +189,21 @@ window.onload = function(){
 					}
 				});
 			}
-			
+
 			var s = cell.speed;
 			if (s == min) cell.className += ' best';
 			else if (s <= min + threshold) cell.className += ' good';
 			else if (s >= max - threshold) cell.className += ' bad';
 			else cell.className += ' normal';
 		});
-		
+
 		if (mismatch){
 			forEach(cells, function(cell, i){
 				if (cell.found != undefined) cell.className += ' mismatch';
 			});
 		}
-		
+
 	};
-	
+
 
 };
---
+++ tests/right2-tests.js
@@ -0,0 +1,139 @@
+window.tests = {
+
+	"make" : function(){
+	  for (var i = 0; i < 250; i++) {
+	    document.body.appendChild(
+	      new Element('ul', {
+	        'class': 'fromcode', id: 'setid'+i
+	      }).insert([
+	        new Element('li', {html: 'one'}),
+	        new Element('li', {html: 'two'}),
+	        new Element('li', {html: 'three'})
+	      ])._
+	    );
+	  }
+
+		return $$('ul.fromcode').length;
+	},
+
+	"indexof" : function(){
+		var index, el, uls;
+
+		for (var i = 0; i < 20; i++) {
+      el    = $('setid150');
+      uls   = $$('ul');
+      index = uls.indexOf(el);
+		}
+
+		return index;
+	},
+
+	"bind" : function(){
+	  var elements = $$('ul > li');
+	  elements.each('on', 'click', function() {});
+		return elements.length;
+	},
+
+	"attr" : function(){
+		return $$("ul").map('id').length;
+	},
+
+	"bindattr" : function(){
+	  var f = function() {};
+	  return $$("ul > li").each(function(element) {
+	    element
+	      .on('mouseover', f)
+  	    .set('rel', 'touched')
+  	    .stopObserving('mouseover', f);
+	  }).length;
+	},
+
+	"table": function(){
+		var table, tr;
+		for (var i = 0; i < 40; i++) {
+			table = new Element('table', { 'class': 'madetable' });
+      document.body.appendChild(table._);
+			tr = new Element('tr');
+			tr.insert(new Element('td'));
+
+			table.insert(tr);
+
+			tr.insert(new Element('td'), 'top');
+		}
+
+		return $$('tr td').length;
+	},
+
+	"addanchor" : function(){
+	  var elements = $$('.fromcode > li');
+	  elements.each(function(li) {
+		  li.insert(new Element('a', { href: 'http://example.com', html: 'link' })._)
+		});
+		return elements.length;
+	},
+
+	"append" : function(){
+	  var div;
+	  for (var i = 0; i < 500; i++) {
+	    div = new Element('div', {rel: 'foo2'});
+	    document.body.appendChild(div._);
+	  }
+
+	  return $$("[rel='foo2']").length;
+	},
+
+	"addclass-odd" : function(){
+	  return $$('div').filter(function(div, index) {
+		  div.addClass('added');
+		  if (index % 2 === 1) return div.addClass('odd');
+	  }).length;
+	},
+
+	"style": function(){
+	  var elements = $$('.added');
+		elements.each('setStyle', {
+			backgroundColor: '#ededed',
+			color: '#fff'
+		});
+		return elements.length;
+	},
+
+	"removeclass": function(){
+	  var elements = $$('.added');
+	  elements.each('removeClass', 'added');
+		return elements.length;
+	},
+
+	"sethtml": function(){
+	  var elements = $$('div');
+	  elements.each('update', "<p>new content</p>");
+		return elements.length;
+	},
+
+	"insertbefore" : function(){
+	  var elements = $$('.fromcode a');
+		elements.each(function(a) {
+		  a.insert(new Element('p', {html: "A Link"}), 'before');
+		});
+		return elements.length;
+	},
+
+	"insertafter" : function(){
+	  var elements = $$(".fromcode a");
+
+		elements.each(function(a) {
+		  a.insert(new Element('p', {html: "After Link"}), 'after');
+		});
+
+		return elements.length;
+	},
+
+	"destroy": function(){
+	    return $$('.fromcode').each('remove').length;
+	},
+
+	"finale": function(){
+	  $(document.body).clean();
+	  return $$("body *").length;
+	}
+};
